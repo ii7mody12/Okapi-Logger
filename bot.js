a7476d6f41b395319344217df454d8a7f19ceb19
NDEogJ <ndeogj@gmail.com>
2018-02-20 03:47:35 -0600

    v4.0

---
+++ bot.js
@@ -0,0 +1,19 @@
+var Discord = require('discord.io');
+
+var bot = new Discord.Client({
+    token: "NDE1MjY5NzczODg3NDA2MDgw.DW18-w.rWFfcZzfeqDIYalhhASvGzfFiss",
+    autorun: true
+});
+
+bot.on('ready', function() {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});

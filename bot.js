9dcd677dbaed5db44abdc8408ab85c8eb2221df1
NDEogJ <ndeogj@gmail.com>
2018-02-19 21:54:30 -0600

    0.1.16

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    bot.user.setGame("online");
+    client.user.setActivity("your every command", {type: "FOLLOWING"});
 });
 
 client.on('message', message => {

801406818f505dab21b12fc9d474f00bee878fb5
NDEogJ <ndeogj@gmail.com>
2018-02-19 20:03:23 -0600

    0.1.2

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDE1MjY5NzczODg3NDA2MDgw.DWzeGA.m9zkjoyXZttkEbSfL3RaLkx3pbQ);
+client.login(process.env.BOT_TOKEN);

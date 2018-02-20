72c987806d6d5c3ab4adc3fae9a3d856b1500f17
NDEogJ <ndeogj@gmail.com>
2018-02-19 20:59:23 -0600

    0.1.6

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-￼ bot.on("ready", async () => {
-￼   console.log(`${bot.user.username} is online!`);
+const client = new Discord.Client();
+￼ client.on("ready", async () => {
+￼   console.log(`${client.user.username} is online!`);
 ￼   bot.user.setActivity("your commands", {type: "Following"});
 ￼   //bot.user.setGame("on SourceCade!");
 ￼ });
-bot.on('message', message => {
+
+client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);

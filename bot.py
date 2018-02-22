776734a5673c951a3b1c8f8db70c71b023488334
NDEogJ <ndeogj@gmail.com>
2018-02-22 07:11:13 -0600

    0.01.02

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 bot = commands.Bot(command_prefix='.')
 
@@ -14,5 +15,11 @@ async def on_ready():
 async def play(ctx, *, play: str):
     await bot.change_presence(game=discord.Game(name=play))
     await ctx.send(f"Thanks to {ctx.author.mention} I'm playing {play}.")
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    t1 = time.perf_counter()
+    t2 = time.perf_counter()
+    await ctx.send("Pong! Round Trip Took: {}ms".format(round((t2-t1)*1000)))
 
 bot.run('NDE1MjY5NzczODg3NDA2MDgw.DW18-w.rWFfcZzfeqDIYalhhASvGzfFiss')

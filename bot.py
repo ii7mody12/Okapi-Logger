580cbb30a1eba1938a1753d0d539621018ede171
NDEogJ <35947029+NDEogJ@users.noreply.github.com>
2018-03-09 09:30:22 -0600

    v34

--- bot.py
+++ bot.py
@@ -32,121 +32,109 @@ async def on_ready():
 
 @bot.command()
 async def play(ctx):
-    async with context.typing():
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name=".playing game name", value="Set Status To Playing")
-        embed.add_field(name=".listening song name", value="Set Status To Listening")
-        embed.add_field(name=".watching video name", value="Set Status To Watching")
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.send(embed=embed)
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name=".playing game name", value="Set Status To Playing")
+    embed.add_field(name=".listening song name", value="Set Status To Listening")
+    embed.add_field(name=".watching video name", value="Set Status To Watching")
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def playing(ctx, *, text: str):
-    async with context.typing():
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=text))
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name=f"I'm Now Playing `{text}`,", value=f"Thanks To {ctx.author.mention}!")
-        await ctx.send(embed=embed)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=text))
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name=f"I'm Now Playing `{text}`,", value=f"Thanks To {ctx.author.mention}!")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def listening(ctx, *, text: str):
-    async with context.typing():
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=text))
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name=f"I'm Now Listening To `{text}`,", value=f"Thanks To {ctx.author.mention}!")
-        await ctx.send(embed=embed)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=text))
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name=f"I'm Now Listening To `{text}`,", value=f"Thanks To {ctx.author.mention}!")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def watching(ctx, *, text: str):
-    async with context.typing():
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=text))
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name=f"I'm Now Watching `{text}`,", value=f"Thanks To {ctx.author.mention}!")
-        await ctx.send(embed=embed)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=text))
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name=f"I'm Now Watching `{text}`,", value=f"Thanks To {ctx.author.mention}!")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
-    async with context.typing():
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name="Pong! <a:loading:418291364221157386>", value=f"Latency: `{bot.latency * 1000:.0f}ms`")
-        await ctx.send(embed=embed)
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name="Pong! <a:loading:418291364221157386>", value=f"Latency: `{bot.latency * 1000:.0f}ms`")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def about(ctx, user: discord.Member):
-    async with context.typing():
-        member = user
-        roles = [role.name.replace('@', '@\u200b') for role in member.roles]
-        roleslist = ', '.join(roles) if len(roles) < 10 else f'{len(roles)} roles'
-        created = user.created_at.strftime("%A %B %d, %Y")
-        embed = discord.Embed(color=random.choice(colors), description=f"**Name** {user.name}\n**Created** {created}\n**Id** {user.id}\n**Status** {user.status}\n**Roles** {roleslist}")
-        embed.set_thumbnail(url=user.avatar_url)
-        await ctx.send(embed=embed)
+    member = user
+    roles = [role.name.replace('@', '@\u200b') for role in member.roles]
+    roleslist = ', '.join(roles) if len(roles) < 10 else f'{len(roles)} roles'
+    created = user.created_at.strftime("%A %B %d, %Y")
+    embed = discord.Embed(color=random.choice(colors), description=f"**Name** {user.name}\n**Created** {created}\n**Id** {user.id}\n**Status** {user.status}\n**Roles** {roleslist}")
+    embed.set_thumbnail(url=user.avatar_url)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def info(ctx):
-    async with context.typing():
-        embed=discord.Embed(color=random.choice(colors), description="**.botinfo** To See Information About The Bot.\n**.serverinfo** To See Information About The Server.\n**.info** To See This Message.")
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.send(embed=embed)
+    embed=discord.Embed(color=random.choice(colors), description="**.botinfo** To See Information About The Bot.\n**.serverinfo** To See Information About The Server.\n**.info** To See This Message.")
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def serverinfo(ctx):
-    async with context.typing():
-        process = psutil.Process()
-        total_members = sum(1 for _ in bot.get_all_members())
-        total_servers = len(bot.guilds)
-        total_channels = sum(1 for _ in bot.get_all_channels())
-        cpu_usage = process.cpu_percent() / psutil.cpu_count()
-        ram_usage = process.memory_full_info().uss / 1024**2
-        embed = discord.Embed(color=random.choice(colors))
-        embed.add_field(name="#\u20e3 Server Statistics:", value="**Guilds**: {}\n**Channels**: {}\n**Users**: {}".format(total_servers, total_channels, total_members))
-        embed.add_field(name="💻 Resource Usage:", value="CPU: {:.2f} %\nRAM: {:.2f} MiB".format(cpu_usage, ram_usage))
-        py_e = bot.get_emoji(417167904342278155)
-        disc_e = bot.get_emoji(417169255629455371)
-        embed.add_field(name="Running On:", value=f"""{py_e}: {".".join([str(v) for v in version_info[:3]])}  {disc_e}: {get_distribution('discord.py').version} [discord.py]""")
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.send(embed=embed)
+    process = psutil.Process()
+    total_members = sum(1 for _ in bot.get_all_members())
+    total_servers = len(bot.guilds)
+    total_channels = sum(1 for _ in bot.get_all_channels())
+    cpu_usage = process.cpu_percent() / psutil.cpu_count()
+    ram_usage = process.memory_full_info().uss / 1024**2
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(name="#\u20e3 Server Statistics:", value="**Guilds**: {}\n**Channels**: {}\n**Users**: {}".format(total_servers, total_channels, total_members))
+    embed.add_field(name="💻 Resource Usage:", value="CPU: {:.2f} %\nRAM: {:.2f} MiB".format(cpu_usage, ram_usage))
+    py_e = bot.get_emoji(417167904342278155)
+    disc_e = bot.get_emoji(417169255629455371)
+    embed.add_field(name="Running On:", value=f"""{py_e}: {".".join([str(v) for v in version_info[:3]])}  {disc_e}: {get_distribution('discord.py').version} [discord.py]""")
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def say(ctx, *, text):
-    async with context.typing():
-        await ctx.message.delete()
-        await ctx.send(f'"{text}" -Anonymous')
+    await ctx.message.delete()
+    await ctx.send(f'"{text}" -Anonymous')
 
 @bot.command()
 async def embed(ctx, *, text):
-    async with context.typing():
-        await ctx.message.delete()
-        embed=discord.Embed(title=f'"{text}"', description="-Anonymous")
-        await ctx.send(embed=embed)
+    await ctx.message.delete()
+    embed=discord.Embed(title=f'"{text}"', description="-Anonymous")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def emoji(ctx):
-    async with context.typing():
-        embed=discord.Embed(color=random.choice(colors))
-        indexval = 0
-        for indexval in range(25):
-            embed.add_field(inline=True, value=emoji_name1[indexval], name=emoji_id1[indexval])
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.message.author.send(embed=embed)
-        embed=discord.Embed(color=random.choice(colors))
-        indexval = 0
-        for indexval in range(25):
-            embed.add_field(inline=True, value=emoji_name2[indexval], name=emoji_id2[indexval])
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.message.author.send(embed=embed)
-        embed=discord.Embed(color=random.choice(colors))
-        embed.add_field(inline=False, value="There You Go!", name="<:blob0w0:417370606921711616>")
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.message.author.send(embed=embed)
-        embed=discord.Embed(description=f"{ctx.author.mention} Check Your DM's!",color=random.choice(colors))
-        await ctx.send(embed=embed)
+    embed=discord.Embed(color=random.choice(colors))
+    indexval = 0
+    for indexval in range(25):
+        embed.add_field(inline=True, value=emoji_name1[indexval], name=emoji_id1[indexval])
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.message.author.send(embed=embed)
+    embed=discord.Embed(color=random.choice(colors))
+    indexval = 0
+    for indexval in range(25):
+        embed.add_field(inline=True, value=emoji_name2[indexval], name=emoji_id2[indexval])
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.message.author.send(embed=embed)
+    embed=discord.Embed(color=random.choice(colors))
+    embed.add_field(inline=False, value="There You Go!", name="<:blob0w0:417370606921711616>")
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.message.author.send(embed=embed)
+    embed=discord.Embed(description=f"{ctx.author.mention} Check Your DM's!",color=random.choice(colors))
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def help(ctx):
-    async with context.typing():
-        embed=discord.Embed(color=random.choice(colors), description="**.help** Explains how to use commands.\n**.info** Tells what the info commands are.\n**.play** Tells what the play commands are.\n**.say text** Says what you want it to.\n**.embed text** Says what you want it to in an embed.\n**.ping** Checks latency of the bot.\n**.emoji** Shows a list of all the ndeogj emojis.\n**.about @username#0000** Tells about a user.")
-        embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
-        await ctx.send(embed=embed)
+    embed=discord.Embed(color=random.choice(colors), description="**.help** Explains how to use commands.\n**.info** Tells what the info commands are.\n**.play** Tells what the play commands are.\n**.say text** Says what you want it to.\n**.embed text** Says what you want it to in an embed.\n**.ping** Checks latency of the bot.\n**.emoji** Shows a list of all the ndeogj emojis.\n**.about @username#0000** Tells about a user.")
+    embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
+    await ctx.send(embed=embed)
 
 bot.run('NDE1MjY5NzczODg3NDA2MDgw.DW18-w.rWFfcZzfeqDIYalhhASvGzfFiss')
